ccaf504354844977079067fbcfc2c3873adcf8a4
106598045 <32007271+106598045@users.noreply.github.com>
2023-07-16 22:33:09 +0800

    Change issue API source

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from urllib.parse import quote
 from langchain.document_loaders import DataFrameLoader
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -13,46 +12,47 @@ from langchain.memory import ConversationBufferMemory
 
 os.environ["OPENAI_API_KEY"] = ""
 def call_api(url):
-    headers = {"Accept": "application/vnd.github.v3+json"}
+    headers = {"Accept": "application/json"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response
     else:
         print("Failed to retrieve issues.")
 
-def search_github_issue_commands(issue_no):
-    url = f"https://api.github.com/repos/quartz-scheduler/quartz/issues/{issue_no}/comments"
-    response_json = call_api(url).json()
-    res = []
-    for command in response_json:
+def parse_jira_comments(json):
+    res = list()
+    for comment in json["comments"]:
         item = {
-            "command_content": command["body"]
+            "body": comment["body"],
+            "author": comment["author"]["displayName"]
         }
         res.append(item)
     return res
 
-def search_github_issues(keyword):
-    url = f"https://api.github.com/repos/quartz-scheduler/quartz/issues?q={quote(keyword)}&per_page=100"
+def search_jira_issue(keyword):
+    # fields => summary, comment, description
+    # jql => project = Java Driver And type = Bug
+    url = f"https://jira.mongodb.org/rest/api/2/search?fields=comment%2csummary%2cdescription&jql=project%20%3D%20%22Java%20Driver%22%20AND%20type%20%3D%20Bug%20AND{quote(keyword)}"
     print("url", url)
     response_json = call_api(url).json()
     res = list()
-    for issue in response_json:
+    for issue in response_json["issues"]:
         item = {
-            "issue_title": issue["title"] or "",
-            "issue_description": issue["body"] or "",
-            "issue_link": issue["html_url"],
-            "issue_state": issue["state"],
-            # "issue_commands": search_github_issue_commands(issue["number"])
+            "issue_summary": issue["fields"]["summary"] or "",
+            "issue_description": issue["fields"]["description"] or "",
+            "issue_link": "https://jira.mongodb.org/browse/"+issue["key"],
+            # "issue_state": issue["state"],
+            "issue_comments": str(parse_jira_comments(issue["fields"]["comment"]))
         }
         res.append(item)
     return res
 
 
-issues = search_github_issues("is:close")
+issues = search_jira_issue('project = "Java Driver" AND type = Bug AND summary ~ "NullPointerException"')
 print("issue count: ", len(issues))
 
 df = pd.DataFrame(issues)
-loader = DataFrameLoader(df, page_content_column="issue_description")
+loader = DataFrameLoader(df, page_content_column="issue_summary")
 docs = loader.load()
 
 embeddings = OpenAIEmbeddings()
@@ -61,12 +61,12 @@ vectorstore = Chroma.from_documents(docs, embeddings)
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 qa = ConversationalRetrievalChain.from_llm(
-    ChatOpenAI(temperature=0.6, model_name="gpt-3.5-turbo"),
+    ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo"),
     vectorstore.as_retriever(max_tokens_limit=4097),
     memory=memory,
     verbose=True
 )
 
-q_1 = "Find issues related to service restart and summarize their root causes."
+q_1 = "Find issues related to NPE and summarize their root causes."
 result = qa({"question": q_1})
 print(result)
